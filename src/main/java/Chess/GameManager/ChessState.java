c6b11a6fd86d9e20af902eb0b7c0f32fad9329d7
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-21 15:31:38 +0200

    puzzle.state changed to puzzle.twophasemovestate

--- src/main/java/Chess/GameManager/ChessState.java
+++ src/main/java/Chess/GameManager/ChessState.java
@@ -1,38 +1,87 @@
 package Chess.GameManager;
 
-import puzzle.State;
+import puzzle.TwoPhaseMoveState;
+import Chess.GameManager.Positions.*;
 
 import java.util.Set;
 
-public class ChessState implements State<ChessPieceMove>
+public class ChessState implements TwoPhaseMoveState<ChessPieceMove>
 {
+    private Position targetPosition;
+    private KnightPosition knightPosition;
+    private KingPosition kingPosition;
+    private boolean kingsTurn;
+
+    public ChessState()
+    {
+        targetPosition = new Position(0,0);
+        knightPosition = new KnightPosition(0,0);
+        kingPosition = new KingPosition(0,0);
+
+        kingsTurn = false;
+    }
+
+    @Override
+    public boolean isLegalToMoveFrom(ChessPieceMove chessPieceMove)
+    {
+        return false;
+    }
 
     @Override
     public boolean isSolved()
     {
-        return false;
+        return targetPosition.equals(knightPosition) || targetPosition.equals(kingPosition);
     }
 
     @Override
-    public boolean isLegalMove(ChessPieceMove chessPieceMove)
+    public boolean isLegalMove(TwoPhaseMove<ChessPieceMove> chessPieceMoveTwoPhaseMove)
     {
-        return false;
+        if(kingPosition.equals(chessPieceMoveTwoPhaseMove.from()))
+        {
+            return switch(chessPieceMoveTwoPhaseMove.from())
+            {
+                case KingDown -> true;
+                case KingDownLeft -> true;
+                case KingDownRight -> true;
+                case KingUp -> true;
+                case KingUpLeft -> true;
+                case KingUpRight -> true;
+                case KingLeft -> true;
+                case KingRight -> true;
+                default -> false;
+            };
+        }
+        else
+        {
+            return switch(chessPieceMoveTwoPhaseMove.from())
+            {
+                case KnightUpUpperLeft -> true;
+                case KnightUpLowerLeft -> true;
+                case KnightUpUpperRight -> true;
+                case KnightUpLowerRight -> true;
+                case KnightDownUpperLeft -> true;
+                case KnightDownLowerLeft -> true;
+                case KnightDownUpperRight -> true;
+                case KnightDownLowerRight -> true;
+                default -> false;
+            };
+        }
     }
 
     @Override
-    public void makeMove(ChessPieceMove chessPieceMove)
+    public void makeMove(TwoPhaseMove<ChessPieceMove> chessPieceMoveTwoPhaseMove)
     {
 
     }
 
     @Override
-    public Set<ChessPieceMove> getLegalMoves()
+    public Set<TwoPhaseMove<ChessPieceMove>> getLegalMoves()
     {
         return Set.of();
     }
 
     @Override
-    public State<ChessPieceMove> clone()
+    public TwoPhaseMoveState<ChessPieceMove> clone()
     {
         return null;
     }
