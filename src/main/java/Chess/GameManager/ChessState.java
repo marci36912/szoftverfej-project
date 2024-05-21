0c7b9cef34ad34121cd3e695af4bfcf5981eb532
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-21 15:52:30 +0200

    removed the legal move logic

--- src/main/java/Chess/GameManager/ChessState.java
+++ src/main/java/Chess/GameManager/ChessState.java
@@ -10,20 +10,18 @@ public class ChessState implements TwoPhaseMoveState<ChessPieceMove>
     private Position targetPosition;
     private KnightPosition knightPosition;
     private KingPosition kingPosition;
-    private boolean kingsTurn;
 
     public ChessState()
     {
         targetPosition = new Position(0,0);
         knightPosition = new KnightPosition(0,0);
         kingPosition = new KingPosition(0,0);
-
-        kingsTurn = false;
     }
 
     @Override
     public boolean isLegalToMoveFrom(ChessPieceMove chessPieceMove)
     {
+
         return false;
     }
 
@@ -36,36 +34,7 @@ public class ChessState implements TwoPhaseMoveState<ChessPieceMove>
     @Override
     public boolean isLegalMove(TwoPhaseMove<ChessPieceMove> chessPieceMoveTwoPhaseMove)
     {
-        if(kingPosition.equals(chessPieceMoveTwoPhaseMove.from()))
-        {
-            return switch(chessPieceMoveTwoPhaseMove.from())
-            {
-                case KingDown -> true;
-                case KingDownLeft -> true;
-                case KingDownRight -> true;
-                case KingUp -> true;
-                case KingUpLeft -> true;
-                case KingUpRight -> true;
-                case KingLeft -> true;
-                case KingRight -> true;
-                default -> false;
-            };
-        }
-        else
-        {
-            return switch(chessPieceMoveTwoPhaseMove.from())
-            {
-                case KnightUpUpperLeft -> true;
-                case KnightUpLowerLeft -> true;
-                case KnightUpUpperRight -> true;
-                case KnightUpLowerRight -> true;
-                case KnightDownUpperLeft -> true;
-                case KnightDownLowerLeft -> true;
-                case KnightDownUpperRight -> true;
-                case KnightDownLowerRight -> true;
-                default -> false;
-            };
-        }
+        return false;
     }
 
     @Override
