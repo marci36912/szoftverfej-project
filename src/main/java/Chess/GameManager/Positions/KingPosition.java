d74fe9906e65352a37fbd3abc51848485a257483
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-21 14:19:10 +0200

    king moves implemented

--- src/main/java/Chess/GameManager/Positions/KingPosition.java
+++ src/main/java/Chess/GameManager/Positions/KingPosition.java
@@ -1,6 +1,98 @@
 package Chess.GameManager.Positions;
 
+import Chess.GameManager.ChessPieceMove;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class KingPosition extends Position
 {
+    public KingPosition(int row, int col)
+    {
+        super(row, col);
+    }
 
+    /***
+     * {@return returns the new position of the king, if it moved up}
+     */
+    public Position MoveUp()
+    {
+        return move(ChessPieceMove.KingUp);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved one up and one right}
+     */
+    public Position MoveUpRight()
+    {
+        return move(ChessPieceMove.KingUpRight);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved one up and one left}
+     */
+    public Position MoveUpLeft()
+    {
+        return move(ChessPieceMove.KingUpRight);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved down}
+     */
+    public Position MoveDown()
+    {
+        return move(ChessPieceMove.KingDown);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved one down and one right}
+     */
+    public Position MoveDownRight()
+    {
+        return move(ChessPieceMove.KingDownRight);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved one down and one left}
+     */
+    public Position MoveDownLeft()
+    {
+        return move(ChessPieceMove.KingDownRight);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved left}
+     */
+    public Position MoveLeft()
+    {
+        return move(ChessPieceMove.KingLeft);
+    }
+
+    /***
+     * {@return returns the new position of the king, if it moved right}
+     */
+    public Position MoveRight()
+    {
+        return move(ChessPieceMove.KingRight);
+    }
+
+    /***
+     * {@return returns a set of every possible position, where the king can move}
+     */
+    @Override
+    public Set<Position> GetAllMoves()
+    {
+        var moves = new HashSet<Position>();
+
+        moves.add(MoveUp());
+        moves.add(MoveUpRight());
+        moves.add(MoveUpLeft());
+        moves.add(MoveDown());
+        moves.add(MoveDownRight());
+        moves.add(MoveDownLeft());
+        moves.add(MoveLeft());
+        moves.add(MoveRight());
+
+        return moves;
+    }
 }
--- src/main/java/Chess/GameManager/Positions/Position.java
+++ src/main/java/Chess/GameManager/Positions/Position.java
@@ -1,6 +1,56 @@
 package Chess.GameManager.Positions;
 
+import Chess.GameManager.ChessPieceMove;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Position
 {
+    protected int row;
+    protected int col;
 
+    public Position(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        Position position = (Position) o;
+        return row == position.row && col == position.col;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(row, col);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("(%d,%d)", row, col);
+    }
+
+    protected Position move(ChessPieceMove move)
+    {
+        return new Position(row+move.getRowChange(), col+move.getColChange());
+    }
+
+    protected Set<Position> GetAllMoves()
+    {
+        return null;
+    }
 }
