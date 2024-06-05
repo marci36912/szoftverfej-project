fbc0a678884dbecd0ffced6fb7fe53df0463381f
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-06-05 09:52:33 +0200

    chessboard auto size done

--- src/main/java/Chess/UI/GameController.java
+++ src/main/java/Chess/UI/GameController.java
@@ -4,11 +4,13 @@ package Chess.UI;
 import Chess.GameManager.ChessState;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class GameController
 {
@@ -26,11 +28,26 @@ public class GameController
         int size = ChessState.BOARD_SIZE * ChessState.BOARD_SIZE;
         labels = new Label[size];
 
+
+
+        for (int i = 0; i < ChessState.BOARD_SIZE; i++)
+        {
+            float s = (float)GameSpace.getPrefHeight() / ChessState.BOARD_SIZE;
+
+            var r = new RowConstraints(s);
+            r.setVgrow(Priority.ALWAYS);
+            var c = new ColumnConstraints(s);
+            c.setHgrow(Priority.ALWAYS);
+
+            GameSpace.getRowConstraints().add(r);
+            GameSpace.getColumnConstraints().add(c);
+        }
+
         for(int i = 0; i < size; i++)
         {
-            labels[i] = new Label("a");
-            labels[i].setPadding(new Insets(10, 10, 10, 10));
-            labels[i].setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
+            labels[i] = new Label(String.valueOf(i));
+           // labels[i].setPadding(new Insets(10, 10, 10, 10));
+          //  labels[i].setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
 
             if(GameSpace != null)
             {
