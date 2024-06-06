b4739cbc0eeff12b3b0fd06d4d03e08ae7e188b9
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-06-06 09:25:35 +0200

    jackson added to the pom.xml

--- src/main/java/Chess/Saves/DataHolder.java
+++ src/main/java/Chess/Saves/DataHolder.java
@@ -1,5 +1,9 @@
 package Chess.Saves;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataHolder
 {
+    public static List<GameInfo> GameInfos = new ArrayList<GameInfo>();
 }
--- src/main/java/Chess/Saves/JsonWriter.java
+++ src/main/java/Chess/Saves/JsonWriter.java
@@ -1,5 +1,13 @@
 package Chess.Saves;
 
+import java.io.File;
+import java.util.List;
+
 public class JsonWriter
 {
+    public void writeSaves(File outputFile, List<GameInfo> games)
+    {
+        ObjectMapper
+    }
+
 }
