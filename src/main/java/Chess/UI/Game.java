d7da9c5b10cc129d7335d2e676fe760b9a8001df
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-06-08 07:55:36 +0200

    properties added to the main stage

--- src/main/java/Chess/UI/Game.java
+++ src/main/java/Chess/UI/Game.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,10 @@ public class Game extends Application
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 
         stage.setScene(new Scene(root));
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        stage.titleProperty().setValue("Chess game");
+        Image king =  new Image(getClass().getResourceAsStream("king.png"));
+        stage.getIcons().add(king);
         stage.show();
     }
 
